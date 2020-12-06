12de9e6fc0273871aa001e8e54d741cb1d5a6c7b
Qian Tang <q.tang6@student.unimelb.edu.au>
2020-12-07 10:21:34 +1100

    learn event handle, data binding, computed properties

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
 new Vue({
   el:"#vue-app",
   data:{
-    name:"Qian",
-    job:"Learning"
+    // name:"Qian",
+    // job:"Learning",
+    // website:"https://unpkg.com/vue",
+    // websiteTag:'<a href="https://unpkg.com/vue">Vue</a>'
+    name:'',
+    age:20,
+    a:0,
+    b:0
+    // x:0,
+    // y:0
   },
   methods:{
-    greet: function(time){
-      // if use ()=>{} syntax, this keyword not working
-      return "Good "+ time + ' !!!!!' + this.name;
+    // greet: function(time){
+    //   // if use ()=>{} syntax, this keyword not working
+    //   return "Good "+ time + ' !!!!!' + this.name;
+    // }
+    // add: function(inc){
+    //   this.age+=inc
+    // },
+    // subtract:function(dec){
+    //   this.age-=dec
+    // },
+    // updateXY:function(event){
+    //   // console.log(event)
+    //   this.x = event.offsetX;
+    //   this.y = event.offsetY;
+    // },
+    // click: function(){
+    //   alert('you clicked me')
+    // }
+    // addToA:function(){
+    //   console.log('AAAA')
+    //   return this.a + this.age
+    // },
+    //
+    // addToB:function(){
+    //   console.log('BBB')
+    //   return this.b + this.age
+    // }
+
+  },
+  computed:{
+    addToA:function(){
+      console.log('AAAA')
+      return this.a + this.age
+    },
+
+    addToB:function(){
+      console.log('BBB')
+      return this.b + this.age
     }
   }
 //TODO: check greet method
